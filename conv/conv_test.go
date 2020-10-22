79499542369a7c546502e14a998cad60a890e66f
wuyu <wuyu@yunify.com>
2020-10-22 16:39:10 +0800

    Add test case for conv

---
+++ conv/conv_test.go
@@ -0,0 +1,17 @@
+package conv
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBytesToString(t *testing.T) {
+	b1 := []byte("Hello World!")
+	require.Equal(t, string(b1), BytesToString(b1))
+}
+
+func TestStringToBytes(t *testing.T) {
+	s1 := "Hello World!"
+	require.Equal(t, []byte(s1), StringToBytes(s1))
+}
